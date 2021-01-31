6944861415f4591ccae3a13a6594efca746d1adb
Jackallster <77934946+Jackallster@users.noreply.github.com>
2021-01-31 18:59:53 +0300

    Classdeneme

---
+++ Musteriler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Musteriler
+    {
+        
+        public string adi { get; set; }
+        public string soyadi { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Musteriler ekle = new Musteriler();
+            ekle.adi = "ataberk";
+            ekle.soyadi = "can";
+
+            Musteriler ekle1 = new Musteriler();
+            ekle1.adi = "veli";
+            ekle1.soyadi = "karan";
+
+            Console.Write("isim :");
+            string ad=Console.ReadLine();
+            Console.Write("soyisim :");
+            string soyisim = Console.ReadLine();
+            Console.WriteLine("--------------");
+            Musteriler ekle2 = new Musteriler();
+            ekle2.adi = ad;
+            ekle2.soyadi = soyisim;
+            
+
+
+            Musteriler[] dizi=new Musteriler[]{ekle,ekle1,ekle2};
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                Console.WriteLine(dizi[i].adi+" "+dizi[i].soyadi);
+            }
+            
+            Console.ReadKey();
+            
+        }
+      
+        }
+    }
+
